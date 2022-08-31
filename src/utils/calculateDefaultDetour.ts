5293c6efe9c144aa29c1edf4dfd36e47e77ce75b
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-08-31 16:36:59 -0300

    corrigindo calculo de desvio padrão

--- src/utils/calculateDefaultDetour.ts
+++ src/utils/calculateDefaultDetour.ts
@@ -16,17 +16,25 @@ export const calculateDefaultDetour = (data: number[]): number => {
      * menos a média do array
      * elevado ao quadrado
      */
-    auxArr[index] = (data[index] - average) * (data[index] - average);
+    auxArr[index] = Math.pow(data[index] - average, 2);
   }
+
+  let lenght: number;
+
+  if (data.length > 30) {
+    lenght = data.length;
+  } else {
+    lenght = data.length - 1;
+  }
+
+  const sum = auxArr.reduce((sum, index) => sum + index, 0);
 
   /**
    * Raiz quadrada da soma
    * de todos os itens do array dividido
    * pela quantidade
    */
-  const result = Math.sqrt(
-    auxArr.reduce((sum, index) => sum + index, 0) / data.length
-  );
-
+  const squareRoot = sum / lenght;
+  const result = Math.sqrt(squareRoot);
   return result;
 };
